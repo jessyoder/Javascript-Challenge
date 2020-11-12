1cf1c1ac281dde38e54e3e09d37dea28b428f337
Jess Yoder <jessica.m.yoder@gmail.com>
2020-11-12 09:56:09 -0500

    updating README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Javascript-Challenge
+
+# JavaScript Homework - JavaScript and DOM Manipulation
+
+## Background
+
+I was given a data collection is too large to search through manually. The website's dedicated followers are complaining that they are having trouble locating specific reports in this mess.
+
+I have been tasked with writing code that will create a table dynamically based upon a dataset provided. Users also need to filter the table data for specific values. 
+
+
+
+### Level 1: Automatic Table and Date Search (Required)
+
+* I created a basic HTML web page.
+
+* Using the UFO dataset provided in the form of an array of JavaScript objects, I wrote code that appended a table to the web page and then added new rows of data for each UFO sighting.
+
+* I used a date form in my HTML document and wrote JavaScript code that will listen for events and search through the columns to find rows that match user input, using these columns:
+
+  1. `date/time`
+  2. `city`
+  3. `state`
+  4. `country`
+  5. `shape`
--- UFO-level-1-2/static/js/app.js
+++ UFO-level-1-2/static/js/app.js
@@ -1,9 +1,10 @@
+// Level 1 
+
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-// Level 1 
+// Append a table to tbe web page
+// Add new rows of data for each UFO sighting
 
 // Select HTML Element to create a list
 var tbody = d3.select("tbody");
@@ -17,122 +18,122 @@ tableData.forEach((UFOSightings) => {
     });
 });
 
-//  Use a date form that will listen for events and search through 
-// the `date/time` column to find rows that match user input.
+// // Create an event listener that will search through the columns to find matching input
+
+// // Select HTML Elements that hold filter button and input field
+// var button = d3.select("#filter-btn");
+// var inputField = d3.select("#datetime");
+
+// button.on("click", function() {
+//     d3.event.preventDefault();
+//     var input = inputField.property("value");
+//     // console.log(input); 
+//     // Create Filtered dataset based on InputValue entered by user
+//     var filteredUFO = tableData.filter(UFOSightings => UFOSightings.datetime === input);
+//     // console.log(filteredUFO);
+//     //Clear all previuos data from UFO table
+//     tbody.html(""); 
+//     // Append new filtered data into a table
+//     filteredUFO.forEach((incident) => {
+//         var row = tbody.append("tr");
+//         Object.entries(incident).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
+// });
+
+// // -------------------------------------------------------------------------------
+
+// // Part 2
+
+// // Create Event Listeners for every column
+
+// var cityButton = d3.select("#city-btn");
+// var cityInputField = d3.select("#cityfilter");
+
+// cityButton.on("click", function() {
+//     d3.event.preventDefault();
+//     var cityInput = cityInputField.property("value");
+//     console.log(cityInput);
+//     // Create Filtered dataset based on InputValue entered by user
+//     var filteredCity = tableData.filter(UFOSightings => UFOSightings.city === cityInput);
+//     console.log(filteredCity); 
+//     //Clear all previuos data from UFO table
+//     tbody.html(""); 
+//     // Append new filtered data into a table
+//     filteredCity.forEach((incident) => {
+//         var row = tbody.append("tr");
+//         Object.entries(incident).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
+// });
+
+// var stateButton = d3.select("#state-btn");
+// var stateInputField = d3.select("#statefilter");
+
+// stateButton.on("click", function() {
+//     d3.event.preventDefault();
+//     var stateInput = stateInputField.property("value");
+//     console.log(stateInput);
+//     // Create Filtered dataset based on InputValue entered by user
+//     var filteredState = tableData.filter(UFOSightings => UFOSightings.state === stateInput);
+//     console.log(filteredState); 
+//     //Clear all previuos data from UFO table
+//     tbody.html(""); 
+//     // Append new filtered data into a table
+//     filteredState.forEach((incident) => {
+//         var row = tbody.append("tr");
+//         Object.entries(incident).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
+// });
+
+// var shapeButton = d3.select("#shape-btn");
+// var shapeInputField = d3.select("#shapefilter");
+
+// shapeButton.on("click", function() {
+//     d3.event.preventDefault();
+//     var shapeInput = shapeInputField.property("value");
+//     console.log(shapeInput);
+//     // Create Filtered dataset based on InputValue entered by user
+//     var filteredShape = tableData.filter(UFOSightings => UFOSightings.shape === shapeInput);
+//     console.log(filteredShape); 
+//     //Clear all previuos data from UFO table
+//     tbody.html(""); 
+//     // Append new filtered data into a table
+//     filteredShape.forEach((incident) => {
+//         var row = tbody.append("tr");
+//         Object.entries(incident).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
+// });
 
 
-// Select HTML Elements that hold filter button and input field
-var button = d3.select("#filter-btn");
-var inputField = d3.select("#datetime");
+// var countryButton = d3.select("#country-btn");
+// var countryInputField = d3.select("#countryfilter");
 
-button.on("click", function() {
-    d3.event.preventDefault();
-    var input = inputField.property("value");
-    // console.log(input); 
-    // Create Filtered dataset based on InputValue entered by user
-    var filteredUFO = tableData.filter(UFOSightings => UFOSightings.datetime === input);
-    // console.log(filteredUFO);
-    //Clear all previuos data from UFO table
-    tbody.html(""); 
-    // Append new filtered data into a table
-    filteredUFO.forEach((incident) => {
-        var row = tbody.append("tr");
-        Object.entries(incident).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
-
-// -------------------------------------------------------------------------------
-
-// Part 2
-
-var cityButton = d3.select("#city-btn");
-var cityInputField = d3.select("#cityfilter");
-
-cityButton.on("click", function() {
-    d3.event.preventDefault();
-    var cityInput = cityInputField.property("value");
-    console.log(cityInput);
-    // Create Filtered dataset based on InputValue entered by user
-    var filteredCity = tableData.filter(UFOSightings => UFOSightings.city === cityInput);
-    console.log(filteredCity); 
-    //Clear all previuos data from UFO table
-    tbody.html(""); 
-    // Append new filtered data into a table
-    filteredCity.forEach((incident) => {
-        var row = tbody.append("tr");
-        Object.entries(incident).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
-
-var stateButton = d3.select("#state-btn");
-var stateInputField = d3.select("#statefilter");
-
-stateButton.on("click", function() {
-    d3.event.preventDefault();
-    var stateInput = stateInputField.property("value");
-    console.log(stateInput);
-    // Create Filtered dataset based on InputValue entered by user
-    var filteredState = tableData.filter(UFOSightings => UFOSightings.state === stateInput);
-    console.log(filteredState); 
-    //Clear all previuos data from UFO table
-    tbody.html(""); 
-    // Append new filtered data into a table
-    filteredState.forEach((incident) => {
-        var row = tbody.append("tr");
-        Object.entries(incident).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
-
-var shapeButton = d3.select("#shape-btn");
-var shapeInputField = d3.select("#shapefilter");
-
-shapeButton.on("click", function() {
-    d3.event.preventDefault();
-    var shapeInput = shapeInputField.property("value");
-    console.log(shapeInput);
-    // Create Filtered dataset based on InputValue entered by user
-    var filteredShape = tableData.filter(UFOSightings => UFOSightings.shape === shapeInput);
-    console.log(filteredShape); 
-    //Clear all previuos data from UFO table
-    tbody.html(""); 
-    // Append new filtered data into a table
-    filteredShape.forEach((incident) => {
-        var row = tbody.append("tr");
-        Object.entries(incident).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
-
-
-var countryButton = d3.select("#country-btn");
-var countryInputField = d3.select("#countryfilter");
-
-countryButton.on("click", function() {
-    d3.event.preventDefault();
-    var countryInput = countryInputField.property("value");
-    console.log(countryInput);
-    // Create Filtered dataset based on InputValue entered by user
-    var filteredCountry = tableData.filter(UFOSightings => UFOSightings.country === countryInput);
-    console.log(filteredCountry); 
-    //Clear all previuos data from UFO table
-    tbody.html(""); 
-    // Append new filtered data into a table
-    filteredCountry.forEach((incident) => {
-        var row = tbody.append("tr");
-        Object.entries(incident).forEach(([key, value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-});
+// countryButton.on("click", function() {
+//     d3.event.preventDefault();
+//     var countryInput = countryInputField.property("value");
+//     console.log(countryInput);
+//     // Create Filtered dataset based on InputValue entered by user
+//     var filteredCountry = tableData.filter(UFOSightings => UFOSightings.country === countryInput);
+//     console.log(filteredCountry); 
+//     //Clear all previuos data from UFO table
+//     tbody.html(""); 
+//     // Append new filtered data into a table
+//     filteredCountry.forEach((incident) => {
+//         var row = tbody.append("tr");
+//         Object.entries(incident).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
+// });
